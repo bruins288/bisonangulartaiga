1fd42cb531c171f389370928ece8e21818ec1b37
bruins288 <bruins288@gmail.com>
2023-02-18 19:47:18 +0300

    тестирование методов post

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
 
