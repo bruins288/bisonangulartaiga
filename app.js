1ec9c092e62d2b45c17d3b95a9cd8018819afa2f
bruins288 <bruins288@gmail.com>
2023-02-18 19:52:07 +0300

    подключение cors и morgan логирование

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
 const authRoutes = require("./routes/auth");
 
+app.use(morgan("dev"));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
